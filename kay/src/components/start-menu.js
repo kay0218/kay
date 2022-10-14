7bf4ed332ab436d30c86a97c269cdc31d2f35611
Kadijah <kallarakhiya@gmail.com>
2022-10-14 11:43:42 +0100

    a href links
    added links to start menu and sorted colors out. menu opens at ideal position

--- kay/src/components/start-menu.js
+++ kay/src/components/start-menu.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import Button from '../../node_modules/@mui/base/ButtonUnstyled';
+// import Button from '../../node_modules/@mui/base/ButtonUnstyled';
 import Menu from '../../node_modules/@mui/base/MenuUnstyled';
 import MenuItem from '../../node_modules/@mui/base/MenuItemUnstyled';
+
+import classes from './start-menu.modules.css';
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -22,19 +24,44 @@ export default function BasicMenu() {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
-        >Start</buttonUnstyledClasses>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        >
+          Start
+        </buttonUnstyledClasses>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+        <MenuItem onClick={handleClose} className="menuitem">
+          <img src='../../images/folders-icon.png' alt='folders icon'/>
+          Folder
+        </MenuItem>
+        <MenuItem onClick={handleClose} className="menuitem">
+        <a href='https://twitter.com/seokayy' target='_blank' alt='Link to my twitter account'>
+        <img src='../../images/twitter-icon.png' alt='twitter icon'/>
+          Twitter
+        </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} className="menuitem">
+        <a href='https://www.linkedin.com/feed/' target='_blank' alt='link to my linkedin'>
+        <img src='../../images/linkedin-icon.png' alt='linkedin icon'/>
+          Linkedin
+        </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} className="menuitem">
+        <a href='https://github.com/kay0218' target='_blank' alt='Link to my github page'>
+        <img src='../../images/github-icon.png' alt='github icon'/>
+          Github
+        </a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} className="menuitem bottommenuitem">
+        <img src='../../images/log-out-icon.png' alt='log out icon'/>
+          Log Out
+        </MenuItem>
       </Menu>
     </div>
   );

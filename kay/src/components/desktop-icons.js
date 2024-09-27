6d2c4226e75edfbc1c73f94d544b5064c3974537
Spooky Scary Skeletons <kallarakhiya@gmail.com>
2024-09-27 22:38:45 +0100

    Outline of dialog box
    Outline created, but there is no styling nor words

--- kay/src/components/desktop-icons.js
+++ kay/src/components/desktop-icons.js
@@ -1,17 +1,31 @@
 import React from "react";
 import '../ui/nav-bg.scss';
-import AboutMeBox from "./about-me-box";
-
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
 
 function DesktopIcons() {
+    const [open, setOpen] = React.useState(false);
+  
+    const handleDialogOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDialogClose = () => {
+      setOpen(false);
+    };
     return (
         <div>
             <div className="icon-one">
-                <button onClick={AboutMeBox} cursor="pointer" >
+                <button onClick={handleDialogOpen} cursor="pointer" >
                     <img id="desktop-icons" src="../images/about-me-icon.png" alt="Icon for about me modal" />
                     <br />
                     <p id="desktop-p-one" >About Me</p>
                 </button>
+                <Dialog open={open}>
+                    <DialogTitle>About Me!</DialogTitle>
+                    <p>lorem ipsum blah blah blah</p>
+                    <button onClick={handleDialogClose}>Close</button>
+                </Dialog>
             </div>
             <br />
             <a target="_blank" href="https://kay0218.github.io/RED/" rel="noreferrer">
